659d86654f73fc867eeda62a042e8254d4508a64
izarc6 <izar.castorina@gmail.com>
2018-12-10 23:27:17 +0100

    Fixed jobs printing & other shieet

--- my_shell.c
+++ my_shell.c
@@ -184,7 +184,7 @@ int execute_line(char *line){
 
 			if (is_bg == 1) {
 				fprintf(stderr,"Añadiendo proceso %d a lista jobs...\n",pid);
-				jobs_list_add(pid,'E',line);
+				jobs_list_add(pid,'E',line_entera);
 			}
 
 			while (jobs_list[0].pid != 0) {
@@ -283,7 +283,6 @@ int parse_args(char **args, char *line) {
 		args[contador] = strtok(path, s);
 		return contador;
 	} else if (strchr(line,'\\')){
-		/*
 		char *aux = strchr(line,'\\');
 		if (aux[1] == ' ') {
 			printf("entramos");
@@ -295,32 +294,6 @@ int parse_args(char **args, char *line) {
 			args[contador] = strtok(path, s);
 			return contador;
 		}
-		*/
-
-		//Leemos el primer token
-		token = strtok(line, s);
-		//Bucle while que lee y guarda los tokens en args
-		while(token != NULL) {
-			args[contador] = token;
-			contador++;
-			token = strtok(NULL, s);
-		}
-		//Si contador es 1 limpiamos el siguiente args para
-		//evitar errores posteriores.
-		if (contador == 1) {
-			args[1] = NULL;
-		}	
-		
-		for (int i=0; i<contador; i++) {
-			if (strchr(args[i],'\\')) {
-				if (args[i+1] != NULL) {
-					strcat(args[i],args[i+1]);
-					strcpy(args[i+1],"");
-				}
-			}
-		}
-
-		return contador;
 	}
 
 	//Leemos el primer token
